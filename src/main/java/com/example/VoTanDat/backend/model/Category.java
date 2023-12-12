c5d63df90887b96dd5faf9898411ee4e963b95ea
VNKaiser <tandatvok16@gmail.com>
2023-12-12 15:16:36 +0700

    [add] model

---
+++ src/main/java/com/example/VoTanDat/backend/model/Category.java
@@ -0,0 +1,26 @@
+package com.example.VoTanDat.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Data
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @Column(name = "category_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    List<Product> products;
+}
